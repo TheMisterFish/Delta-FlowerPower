f9c4527e7a69ab12e4e6a6449d48502ebf96014c
Vincent V <i393504@student.fontys.nl>
2020-11-16 13:01:40 +0100

    Updated simple_detect yolov5 and gitignore
    Former-commit-id: b1b0c28a809367aa718926e10a1505d6d69f1f37

--- field_application/backend/yolov5/simple_detect.py
+++ field_application/backend/yolov5/simple_detect.py
@@ -18,11 +18,11 @@ from utils.plots import plot_one_box
 from utils.torch_utils import select_device, load_classifier, time_synchronized
 
 
-def detect(save_img=False):
-    source, weights, imgsz = opt.source, opt.weights, opt.img_size
+def detect(weights, img_size, conf, source):
+    source, weights, imgsz = source, weights, img_size
     # Initialize
     set_logging()
-    device = select_device(opt.device)
+    device = select_device('')
     half = device.type != 'cpu'  # half precision only supported on CUDA
 
     # Load model
@@ -58,10 +58,10 @@ def detect(save_img=False):
 
         # Inference
         t1 = time_synchronized()
-        pred = model(img, augment=opt.augment)[0]
+        pred = model(img, augment=None)[0]
 
         # Apply NMS
-        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+        pred = non_max_suppression(pred, conf, 0.45, classes=None, agnostic=None)
         t2 = time_synchronized()
 
         # Apply Classifier
@@ -70,6 +70,7 @@ def detect(save_img=False):
 
         # Process detections
         for i, det in enumerate(pred):  # detections per image
+            print(Path(path))
             p, s, im0 = Path(path), '', im0s
 
             s += '%gx%g ' % img.shape[2:]  # print string
@@ -86,7 +87,7 @@ def detect(save_img=False):
                 # Write results
                 for *xyxy, conf, cls in reversed(det):
                     xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                    print(calcBox(xywh))
+                    print(calcBox(xywh, imgsz, imgsz))
 
             # Print time (inference + NMS)
             print('%sDone. (%.3fs)' % (s, t2 - t1))
@@ -116,32 +117,9 @@ def calcBox(xywh, width, height):
     return (t,l),(b,r)
 
 
-# if __name__ == '__main__':
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
-#     parser.add_argument('--source', type=str, default='data/images', help='source')  # file/folder, 0 for webcam
-#     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
-#     parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
-#     parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
-#     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-#     # parser.add_argument('--view-img', action='store_true', help='display results')
-#     # parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-#     # parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
-#     parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
-#     parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-#     parser.add_argument('--augment', action='store_true', help='augmented inference')
-#     parser.add_argument('--update', action='store_true', help='update all models')
-#     parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
-#     opt = parser.parse_args()
-#     print(opt)
-
-#     with torch.no_grad():
-#         if opt.update:  # update all models (to fix SourceChangeWarning)
-#             for opt.weights in ['yolov5s.pt', 'yolov5m.pt', 'yolov5l.pt', 'yolov5x.pt']:
-#                 detect()
-#                 strip_optimizer(opt.weights)
-#         else:
-#             detect()
+if __name__ == '__main__':
+    detect('./weights/yolov5_best.pt', 416, 0.1, './testimgs')
 
 def test():
+    detect('./weights/yolov5_best.pt', 416, 0.1, './testimgs')
     return "It worked!"
