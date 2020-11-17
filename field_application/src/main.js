ed36a0fbb20c5a3d316acb7200124e00a507dfcf
Vincent V <i393504@student.fontys.nl>
2020-11-17 20:56:40 +0100

    Tried adding zeropc to app.js
    Former-commit-id: 5c65ac7ef6483b130f439af54cb46509a16e7ab9

--- field_application/src/main.js
+++ field_application/src/main.js
@@ -1,10 +1,35 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import zeropc from 'zeropc';
+
+const zeropc_client = zeropc;
 
 Vue.config.productionTip = false
 
+let client = new zeropc_client.Client()
+
+client.connect("tcp://127.0.0.1:4242")
+
+client.invoke("echo", "server ready", (error, res) => {
+  if(error || res !== 'server ready') {
+    console.error(error)
+  } else {
+    console.log("server is ready")
+  }
+})
+
+client.invoke("calc", 1, (error, res) => {
+  if(error) {
+    console.error(error)
+  } else {
+    console.log("res:");
+    console.log(res);
+  }
+});
+
 new Vue({
   router,
+  client,
   render: h => h(App)
 }).$mount('#app')
