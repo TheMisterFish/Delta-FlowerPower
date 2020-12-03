22f9ac2dd04cfcb5d0c82f39cd545daa5d457b4c
Vincent V <i393504@student.fontys.nl>
2020-12-03 22:51:04 +0100

    Forgot to fix 2 files.
    Former-commit-id: f4939edf35cdbaada8e177f445cd6e29737d7c3e

--- field_application/src/main.js
+++ field_application/src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from './plugins/vuetify'
-import moment from 'moment' 
+import moment from 'moment'
 import Axios from 'axios';
 
 moment.locale('nl')
@@ -11,13 +11,15 @@ window.moment = moment;
 
 Vue.config.productionTip = false;
 
-Vue.prototype.$vuetify = { rtl: false };
+Vue.prototype.$vuetify = {
+    rtl: false
+};
 Vue.prototype.$http = Axios;
 // localStorage.setItem("token", "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoicmVzZWFyY2hlciIsInJvbGUiOiJyZXNlYXJjaGVyIiwiZW1haWwiOiJyZXNlYXJjaGVyQGZsb3dlcnBvd2VyLmNvbSIsIl9pZCI6IjVmYzYzMTEyYWI2YmQ5MDQ1MWU4M2E4ZSIsImlhdCI6MTYwNzAwODc2MiwiZXhwIjoxNjQzMjk2NzYyfQ.HDSEaruCXUoITF-a00abxkG2HJ_Es1iTSJDNO69oEJc")
 const token = localStorage.getItem("token");
 
 if (token) {
-                Vue.prototype.$http.defaults.headers.common["Authorization"] = token;
+    Vue.prototype.$http.defaults.headers.common["Authorization"] = token;
 }
 
 Vue.component("alert", require("./components/AlertComponent.vue").default, );
@@ -25,13 +27,13 @@ Vue.component("alert", require("./components/AlertComponent.vue").default, );
 // Input components
 const req = require.context('./components/Inputs', true, /\.(js|vue)$/i);
 req.keys().map(key => {
-                const name = key.match(/\w+/)[0];
-                return Vue.component(name, req(key).default)
+    const name = key.match(/\w+/)[0];
+    return Vue.component(name, req(key).default)
 });
 
 new Vue({
-                router,
-                store,
-                vuetify,
-                render: (h) => h(App),
+    router,
+    store,
+    vuetify,
+    render: (h) => h(App),
 }).$mount("#app");
