abcfdbbc42b023db82705d1cfb0e1aefc5f6313b
Vincent V <i393504@student.fontys.nl>
2020-12-03 16:46:47 +0100

    Epic merge
    Former-commit-id: f8e123cfa2ee2155f0d7a07e6a801398e1e89654

--- field_application/src/main.js
+++ field_application/src/main.js
@@ -4,6 +4,7 @@ import router from "./router";
 import store from "./store";
 import vuetify from './plugins/vuetify'
 import moment from 'moment' 
+import Axios from 'axios';
 
 moment.locale('nl')
 window.moment = moment;
