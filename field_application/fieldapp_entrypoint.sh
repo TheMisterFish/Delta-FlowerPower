56814bf5d4779dad549641bf1f0202f6d72729de
Vincent V <i393504@student.fontys.nl>
2020-12-29 16:13:01 +0100

    Testing node

--- field_application/fieldapp_entrypoint.sh
+++ field_application/fieldapp_entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-apt-get update -y
-wine python --version
-wine python -m pip install --upgrade pip
-wine python -m pip --version
-wine python -m pip install setuptools --upgrade
-wine python -m pip install typed_ast‑1.1.0‑cp36‑cp36m‑win_amd64.whl
-wine python -m pip install -r /app/public/backend/requirements.txt
-wine python -m pip list
+# apt-get update -y
+# wine python --version
+# wine python -m pip install --upgrade pip
+# wine python -m pip --version
+# wine python -m pip install setuptools --upgrade
+# wine python -m pip install typed_ast‑1.1.0‑cp36‑cp36m‑win_amd64.whl
+# wine python -m pip install -r /app/public/backend/requirements.txt
+# wine python -m pip list
+wine node -v
