9e74ec8ea5d9fb7e257cc8742c229c685733e917
Vincent V <i393504@student.fontys.nl>
2020-12-29 16:34:43 +0100

    Check NPM

--- field_application/fieldapp_entrypoint.sh
+++ field_application/fieldapp_entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 apt-get update -y
-# wine python --version
-# wine python -m pip install --upgrade pip
-# wine python -m pip --version
-# wine python -m pip install setuptools --upgrade
-# wine python -m pip install typed_ast‑1.1.0‑cp36‑cp36m‑win_amd64.whl
-# wine python -m pip install -r /app/public/backend/requirements.txt
-# wine python -m pip list
+wine python --version
+wine python -m pip install --upgrade pip
+wine python -m pip --version
+wine python -m pip install setuptools --upgrade
+wine python -m pip install typed_ast‑1.1.0‑cp36‑cp36m‑win_amd64.whl
+wine python -m pip install -r /app/public/backend/requirements.txt
+wine python -m pip list
 apt-get install curl software-properties-common -y --fix-missing
 curl -sL https://deb.nodesource.com/setup_12.x | bash -
 apt-get install nodejs -y --fix-missing
 apt-get install npm -y --fix-missing
 node --version
-nodejs --version
+npm --version
