f6e7ec7e210a88e6431be51efa5ca14358b799f6
Vincent V <i393504@student.fontys.nl>
2020-12-29 16:16:15 +0100

    Forgot a -y flag

--- field_application/fieldapp_entrypoint.sh
+++ field_application/fieldapp_entrypoint.sh
@@ -7,6 +7,6 @@
 # wine python -m pip install typed_ast‑1.1.0‑cp36‑cp36m‑win_amd64.whl
 # wine python -m pip install -r /app/public/backend/requirements.txt
 # wine python -m pip list
-apt-get install nodejs
+apt-get install nodejs -y
 node -v
 npm -v 
