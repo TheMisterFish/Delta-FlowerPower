0c1aaf19ed3af8b62c91d7542497e30b3a43f8ae
Vincent V <i393504@student.fontys.nl>
2020-12-29 15:48:31 +0100

    Testing pip install

--- field_application/fieldapp_entrypoint.sh
+++ field_application/fieldapp_entrypoint.sh
@@ -3,5 +3,7 @@ apt-get update -y
 apt-get install -y wget
 wine python --version
 wine python -m pip --version
+wine python -m pip install --upgrade setuptools
+wine python -m pip install typed_ast‑1.1.0‑cp36‑cp36m‑win_amd64.whl
 wine python -m pip install -r /app/public/backend/requirements.txt
 wine python -m pip list
