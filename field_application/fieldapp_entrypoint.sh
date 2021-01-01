7aba600bf3487e1562fa9c32a1efc9a83b9de251
Vincent V <i393504@student.fontys.nl>
2021-01-01 13:35:27 +0100

    Forgot to uncomment requirements

--- field_application/fieldapp_entrypoint.sh
+++ field_application/fieldapp_entrypoint.sh
@@ -3,9 +3,9 @@ apt-get update -y
 wine python --version
 wine python -m pip install --upgrade pip
 wine python -m pip --version
-# wine python -m pip install setuptools --upgrade
-# wine python -m pip install typed_ast‑1.1.0‑cp36‑cp36m‑win_amd64.whl
-# wine python -m pip install -r /app/public/backend/requirements.txt
+wine python -m pip install setuptools --upgrade
+wine python -m pip install typed_ast‑1.1.0‑cp36‑cp36m‑win_amd64.whl
+wine python -m pip install -r /app/public/backend/requirements.txt
 wine python -m pip list
 
 apt-get install curl software-properties-common -y --fix-missing
