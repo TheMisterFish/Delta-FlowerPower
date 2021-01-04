c0cc30d2c7bcd0d00751dfe70ec85975beb58468
Venhuizen,Vincent V.P.J.M <v.venhuizen@student.fontys.nl>
2021-01-04 14:09:20 +0100

    Test 4
    T

---
+++ field_application/src/views/ShowResearch.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        Show Research info en data + upload / verwijder knoppen
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            research_id: {
+                type: Number,
+                required: true
+            }
+        },
+        mounted() {
+            //get research using id
+        },
+    }
+</script>
+
+<style lang="css" scoped>
+
+</style>
